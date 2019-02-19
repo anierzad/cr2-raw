549b5b6df2e4c8692f5f48552d63e0ca91226b60
Adam Nierzad <a.nierzad@gmail.com>
2019-02-19 13:28:42 +0000

    Moved retrieval of image preview.

--- index.js
+++ index.js
@@ -10,4 +10,48 @@ const filePath = path.resolve('test.CR2');
 
 const raw = readraw(filePath);
 
-raw.getPreview();
+// Retrieves the preview image data.
+function previewImage() {
+
+  const stripOffsetTag = 0x0111;
+
+  let offset;
+
+  if (rawData.ifds[0]
+    && rawData.ifds[0][stripOffsetTag]
+    && rawData.ifds[0][stripOffsetTag].tagValue) {
+
+    offset = rawData.ifds[0][stripOffsetTag].tagValue;
+  }
+
+  if (!offset) {
+    return null;
+  }
+
+  let end;
+  let testPos = offset;
+
+  while (!end) {
+
+    const testA = buffer.read(testPos, dataTypes.types.ubyte, 1);
+    const testB = buffer.read(testPos + 1, dataTypes.types.ubyte, 1);
+
+    if (testA === 0xff && testB === 0xd9) {
+
+      end = testPos;
+      break;
+    }
+
+    testPos++;
+  }
+
+  end = end + 2;
+
+  const imgData = buffer.copy(offset, end - offset);
+
+  return Buffer.from(imgData);
+}
+
+module.exports = {
+  previewImage: previewImage
+};
--- readraw/index.js
+++ readraw/index.js
@@ -10,9 +10,7 @@ const bufferFile = require('./buffer-file'),
 function readraw(filePath) {
 
   const rawData = {
-    ifds: {
-
-    }
+    ifds: {}
   };
 
   const buffer = readBuffer(filePath);
@@ -106,50 +104,7 @@ function readraw(filePath) {
     return nextIfd;
   }
 
-  function getPreview() {
-
-    const stripOffsetTag = 0x0111;
-
-    let offset;
-
-    if (rawData.ifds[0]
-      && rawData.ifds[0][stripOffsetTag]
-      && rawData.ifds[0][stripOffsetTag].tagValue) {
-
-      offset = rawData.ifds[0][stripOffsetTag].tagValue;
-    }
-
-    if (!offset) {
-      return null;
-    }
-
-    let end;
-    let testPos = offset;
-
-    while (!end) {
-
-      const testA = buffer.read(testPos, dataTypes.types.ubyte, 1);
-      const testB = buffer.read(testPos + 1, dataTypes.types.ubyte, 1);
-
-      if (testA === 0xff && testB === 0xd9) {
-
-        end = testPos;
-        break;
-      }
-
-      testPos++;
-    }
-
-    end = end + 2;
-
-    const imgData = buffer.copy(offset, end - offset);
-
-    return Buffer.from(imgData);
-  }
-
-  return {
-    getPreview: getPreview
-  }
+  return rawData;
 }
 
 module.exports = readraw;
