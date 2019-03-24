07929d69be5736632f80bcd5c5e80cfe4a0480fd
Adam Nierzad <a.nierzad@gmail.com>
2019-03-24 12:27:34 +0000

    Implemented mmeta-data module.

--- meta-data.js
+++ meta-data.js
@@ -15,6 +15,64 @@ const definitions = {
   }
 };
 
+function fetch(raw, metaDefinition) {
+
+  let result;
+  let searchIfds = [0, 1, 2, 3];
+
+  // IFD specified?
+  if (metaDefinition.ifd !== undefined) {
+
+    // Narrow search.
+    searchIfds = [metaDefinition.ifd];
+  }
+
+  // Loop search IFDs.
+  for (let ifdId in searchIfds) {
+
+    const ifd = getIfd(raw, ifdId);
+    const tag = getTag(ifd, metaDefinition.tagId);
+
+    if (tag && tag.tagValue) {
+
+      result = tag.tagValue;
+      break;
+    }
+  }
+
+  // Do we have a result and is there a parse function?
+  if (result !== undefined
+    && metaDefinition.parse) {
+
+    result = metaDefinition.parse(result);
+  }
+
+  return result;
+}
+
+function getIfd(raw, ifdId) {
+
+  let ifd;
+
+  if (raw.ifds && raw.ifds[ifdId]) {
+    ifd = raw.ifds[ifdId];
+  }
+
+  return ifd;
+}
+
+function getTag(ifd, tagId) {
+
+  let tag;
+
+  if (ifd && ifd[tagId]) {
+    tag = ifd[tagId];
+  }
+
+  return tag;
+}
+
 module.exports = {
-  definitions: definitions
+  definitions: definitions,
+  fetch: fetch
 };
